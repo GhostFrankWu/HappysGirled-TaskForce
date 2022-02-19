229c258710a89c65c47e637aa32e4b3469c4ec0f
Certseeds <51754303+Certseeds@users.noreply.github.com>
2022-02-19 19:52:46 +0800

    ci: actions daily commits
    </subject>
    
    Branch: main
    
    <type>:
    - [ ] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    PS: dont forget update README every a few month to keep actions going on
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- commit.sh
+++ commit.sh
@@ -1,23 +1,23 @@
-begin=$(expr $(cat ./begin) + 86400) 
+#!/usr/bin/env bash
+set -euox pipefail
+
+begin=$(expr $(cat ./begin.date) + 86400) 
 end=$(date '+%s')
 
 list=$(seq $begin 86400 $end)
 after=''
 
-for i in $list
-do
+for i in $list; do
     date -u --date=@$i '+%Y年%m月%d日：没有' >> ./data.md
     echo >> ./data.md
 
     sed -i '1{:a;N;'10'!b a};$d;N;P;D' ./readme.md
     tail ./data.md >> ./readme.md
 
-    echo $i > ./begin
+    echo $i > ./begin.date
     
     git add ./data.md
-    git add ./begin.md
+    # git add ./begin.md
     git add ./readme.md
-    git commit --date=$i --message='happys GKD!'
+    git commit -s --date=$i --message='happys GKD!'
 done
-
-git push origin HEAD
